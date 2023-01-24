fcf10f3a2b194fbcd87cc01686dae5267dc8109f
Omer Sungurr <omersungur615@gmail.com>
2023-01-24 17:22:15 +0300

    Initial commit

---
+++ app/src/main/java/com/omersungur/navigationexample/FirstFragment.kt
@@ -0,0 +1,35 @@
+package com.omersungur.navigationexample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.*
+
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonNext.setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment("omersungur") // 2. fragment bir argüman alıyor.
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
